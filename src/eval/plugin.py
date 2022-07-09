71d86b258341e8b0501aa6ff6bba2686a36ca986
Filip <fszatkowski96@gmail.com>
2022-07-10 01:27:19 +0200

    Setup Naive replay strategy

---
+++ src/eval/plugin.py
@@ -0,0 +1,42 @@
+import omegaconf
+from avalanche.evaluation.metrics import (
+    accuracy_metrics,
+    confusion_matrix_metrics,
+    forgetting_metrics,
+    loss_metrics,
+)
+from avalanche.logging import InteractiveLogger, WandBLogger
+from avalanche.training.plugins import EvaluationPlugin
+
+from config import TDictConfig
+
+
+def get_eval_plugin(cfg: TDictConfig) -> EvaluationPlugin:
+    run_name = f"{cfg.benchmark.name}_{cfg.strategy}_{cfg.model.name}"
+    cfg_dict = omegaconf.OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+
+    interactive_logger = InteractiveLogger()
+    wandb_logger = WandBLogger(project_name=cfg.wandb_project, run_name=run_name, config=cfg_dict)
+    eval_plugin = EvaluationPlugin(
+        accuracy_metrics(
+            minibatch=True,
+            epoch=True,
+            epoch_running=True,
+            experience=True,
+            stream=True,
+        ),
+        loss_metrics(
+            minibatch=True,
+            epoch=True,
+            epoch_running=True,
+            experience=True,
+            stream=True,
+        ),
+        forgetting_metrics(experience=True, stream=True),
+        confusion_matrix_metrics(
+            stream=True, wandb=True, class_names=[str(i) for i in range(cfg.benchmark.n_classes)]
+        ),
+        loggers=[interactive_logger, wandb_logger],
+    )
+
+    return eval_plugin
---
+++ src/models/mlp.py
@@ -0,0 +1,72 @@
+from typing import Optional
+
+import torch.nn as nn
+from avalanche.models.base_model import BaseModel
+from torch import Tensor
+
+
+class MLP(nn.Module, BaseModel):
+    def __init__(
+        self,
+        num_classes: int = 10,
+        input_size: int = 28 * 28,
+        hidden_sizes: Optional[list[int]] = None,
+        dropout_ratio: float = 0.5,
+    ):
+        """
+        Multi-Layer Perceptron with custom parameters and support for partial inference from
+        intermediate layers.
+        :param num_classes: Output size.
+        :param input_size: Input size.
+        :param hidden_sizes: Hidden layer sizes
+        :param dropout_ratio: Dropout rate. 0 to disable.
+        """
+        super().__init__()
+        if hidden_sizes is None:
+            hidden_sizes = [512]
+
+        layers = nn.Sequential()
+        for layer_idx, (in_size, out_size) in enumerate(
+            zip([input_size] + hidden_sizes, hidden_sizes)
+        ):
+            layers.add_module(
+                f"fc{layer_idx}",
+                HiddenLayer(in_size=in_size, out_size=out_size, dropout_ratio=dropout_ratio),
+            )
+        layers.add_module("classifier", nn.Linear(hidden_sizes[-1], num_classes))
+        self.layers = layers
+
+        self._input_size = input_size
+
+    def forward(self, x: Tensor, skip_first: int = 0, skip_last: int = 0) -> Tensor:
+        if len(self.layers) - skip_last - skip_first <= 0:
+            raise ValueError()
+
+        x = x.contiguous()
+        x = x.view(x.size(0), -1)
+
+        for layer in self.layers[skip_first : len(self.layers) - skip_last]:
+            x = layer(x)
+        return x
+
+    def get_features(self, x: Tensor) -> Tensor:
+        return self.forward(x, skip_last=1)
+
+
+class HiddenLayer(nn.Module):
+    def __init__(self, in_size: int, out_size: int, dropout_ratio: float):
+        """
+        Single dense hidden layer with ReLU activation and dropout.
+        :param in_size: Input size.
+        :param out_size: Output size.
+        :param dropout_ratio: Dropout ratio. 0 to disable.
+        """
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(in_size, out_size),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout_ratio),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.net(x)
