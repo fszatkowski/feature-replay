d4126e5127b74fc4caa6b0278548aaec5b1bc672
Filip <fszatkowski96@gmail.com>
2022-08-19 10:50:40 +0200

    feat: Add padding for training SplitMNIST and SplitOmniglot with pooling layers

--- src/benchmarks/utils.py
+++ src/benchmarks/utils.py
@@ -10,12 +10,21 @@ from avalanche.benchmarks.classic.ccifar100 import (
     _default_cifar100_eval_transform,
     _default_cifar100_train_transform,
 )
+from avalanche.benchmarks.classic.cmnist import (
+    _default_mnist_eval_transform,
+    _default_mnist_train_transform,
+)
+from avalanche.benchmarks.classic.comniglot import (
+    _default_omniglot_eval_transform,
+    _default_omniglot_train_transform,
+)
 from avalanche.benchmarks.classic.ctiny_imagenet import (
     _default_eval_transform as _default_imagenet_eval_transform,
 )
 from avalanche.benchmarks.classic.ctiny_imagenet import (
     _default_train_transform as _default_tinyimagenet_train_transform,
 )
+from torchvision.transforms import Pad
 
 from config import Config
 
@@ -35,9 +44,23 @@ def get_benchmark(cfg: Config) -> TBenchmark:
         )
 
     elif cfg.benchmark.name == "SplitMNIST":
+        if cfg.model.pooling:
+            _default_mnist_train_transform.transforms.append(
+                Pad(2, padding_mode="edge")
+            )
+            _default_mnist_eval_transform.transforms.append(Pad(2, padding_mode="edge"))
+            cfg.benchmark.input_size = (1, 32, 32)
         benchmark = SplitMNIST(n_experiences=cfg.benchmark.n_experiences, seed=cfg.seed)
 
     elif cfg.benchmark.name == "SplitOmniglot":
+        if cfg.model.pooling:
+            _default_omniglot_train_transform.transforms.append(
+                Pad((11, 11, 12, 12), padding_mode="edge")
+            )
+            _default_omniglot_eval_transform.transforms.append(
+                Pad((11, 11, 12, 12), padding_mode="edge")
+            )
+            cfg.benchmark.input_size = (1, 128, 128)
         benchmark = SplitOmniglot(
             n_experiences=cfg.benchmark.n_experiences, seed=cfg.seed
         )
@@ -72,9 +95,23 @@ def get_benchmark_for_joint_training(cfg: Config) -> TBenchmark:
         )
 
     elif cfg.benchmark.name == "SplitMNIST":
+        if cfg.model.pooling:
+            _default_mnist_train_transform.transforms.append(
+                Pad(2, padding_mode="edge")
+            )
+            _default_mnist_eval_transform.transforms.append(Pad(2, padding_mode="edge"))
+            cfg.benchmark.input_size = (1, 32, 32)
         benchmark = SplitMNIST(n_experiences=1, seed=cfg.seed)
 
     elif cfg.benchmark.name == "SplitOmniglot":
+        if cfg.model.pooling:
+            _default_omniglot_train_transform.transforms.append(
+                Pad((11, 11, 12, 12), padding_mode="edge")
+            )
+            _default_omniglot_eval_transform.transforms.append(
+                Pad((11, 11, 12, 12), padding_mode="edge")
+            )
+            cfg.benchmark.input_size = (1, 128, 128)
         benchmark = SplitOmniglot(n_experiences=1, seed=cfg.seed)
 
     elif cfg.benchmark.name == "SplitTinyImageNet":
