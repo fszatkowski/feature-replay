4af533f032d3f45504dcd945bfead8bb0d59b6fb
Filip Szatkowski <39523414+fszatkowski@users.noreply.github.com>
2022-10-12 19:54:52 +0200

    Refactor code to enable training without Wandb, update requirements (#28)

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ import hydra
 import numpy as np
 import torch
 
-from benchmarks.utils import get_benchmark, get_benchmark_for_joint_training
+from benchmarks.utils import get_benchmark
 from config import Config
 from models.utils import get_model
 from strategies.utils import get_training_strategy
@@ -24,10 +24,7 @@ def run(cfg: Config):
         random.seed(cfg.seed)
         np.random.seed(cfg.seed)
 
-    if cfg.strategy.name != "JointTraining":
-        benchmark = get_benchmark(cfg)
-    else:
-        benchmark = get_benchmark_for_joint_training(cfg)
+    benchmark = get_benchmark(cfg)
 
     model = get_model(cfg)
     strategy = get_training_strategy(cfg=cfg, model=model)
@@ -36,9 +33,11 @@ def run(cfg: Config):
         strategy.train(experience)
         strategy.eval(benchmark.test_stream)
 
-    if cfg.output_model_path is not None:
-        Path(cfg.output_model_path).parent.mkdir(exist_ok=True, parents=True)
-        torch.save(model, cfg.output_model_path)
+    if cfg.output_dir is not None:
+        output_dir = Path(cfg.output_dir)
+        output_dir.mkdir(exist_ok=True, parents=True)
+        model_path = output_dir / "model.pt"
+        torch.save(model, str(model_path))
 
 
 if __name__ == "__main__":
